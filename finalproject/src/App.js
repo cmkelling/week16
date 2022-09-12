0d03a45120ce9add1286cccaba1c558d9ad35d36
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-12 18:34:55 -0500

    commit

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useRouteMatch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
-function App() {
+function App() { 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/home">Disney 2023</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/hotelFood">Hotel + Food</Nav.Link>
+            <Nav.Link href="/rides">Rides</Nav.Link>
+            <Nav.Link href="/budgeting">Budgeting</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/home'>
+          <Home/>
+        </Route>
+        <Route path='/hotelFood'>
+          <HotelFood/>
+        </Route>
+        <Route path='/rides'>
+          <Rides/>
+        </Route>
+        <Route path='/budgeting'>
+          <Budgeting/>
+        </Route>
+      </Routes>
+    </Router>
+  )
+
+  function Home() {
+    return <h2>Home</h2>
+  };
+
+  function HotelFood() {
+    return <h2>Hotel + Food</h2>
+  };
+
+  function Rides() {
+    return <h2>Rides</h2>
+  };
+
+  function Budgeting() {
+    return <h2>Budgeting</h2>
+  };
+ 
 }
 
 export default App;
