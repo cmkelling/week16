ae942187a1f7ff29b100b46d9fecc7af3efd6ad2
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-15 18:30:07 -0500

    Food is Done!

--- finalproject/src/components/food.js
+++ finalproject/src/components/food.js
@@ -1,7 +1,360 @@
 import React from 'react'
 
+import { Table } from 'react-bootstrap'
+
 export default function Food() {
     return(
-        <h3>Food</h3>
+        <>
+            <h3>Californian Adventure</h3>
+            <Table striped bordered hover size='sm' variant="dark">
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Type of Service</th>
+            <th>Price Range</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>Pym Test Kitchen</td>
+            <td>Quick + Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Pacific Wharf Distribution Co.</td>
+            <td>Mobile Orders</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Poultry Palace</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Lamplight Lounge</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Fiddler, Fifer + Practical Cafe</td>
+            <td>Quick, Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Terran Treats</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Smokejumpers Grill</td>
+            <td>Mobile, Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Flo's V8 Cafe</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Angry Dogs</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Clarabelle's Hand-Scooped Ice Cream</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Lucky Fortune Cookery</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Award Wieners</td>
+            <td>Quick, Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Adorable Snowman Frosted Treats</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Carthay Circle Lounge</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Schmoozies</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Mendocino</td>
+            <td>Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Studio Catering Co.</td>
+            <td>Quick, Mobile</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Cocina Cucamonga Mexican Grill</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Paradise Garden Grill</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Boardwalk Pizza + Pasta</td>
+            <td>Quick, Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Senor Buzz Churros</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Bayside Brews</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Shawarma Palace</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Rita's Baja Blenders</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Wine Country Trattoria</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Cozy Cone Motel</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Jack-Jack Cookie Num Nums</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Corn Dog Castle</td>
+            <td>Quick, Table</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Sonoma Terrace</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Hollywood Lounge</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Pacific Wharf Cafe</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+        </tbody>
+        </Table>
+        <br/>
+        <h3>Disneyland Park</h3>
+            <Table striped bordered hover size='sm' variant="dark">
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Type of Service</th>
+            <th>Price Range</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>Oga's Cantina</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Docking Bay 7 Food and Cargo</td>
+            <td>Quick, Mobile</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Red Rose Taverne</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Market House</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>The Golden Horshoe</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Ronto Roasters</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Pluto's Dog House</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Galactic Grill</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>River Belle Terrace</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Kat Saka's Kettle</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>The Tropical Hideaway</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Mint Julep Bar</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Hungry Bear Restaurant</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>French Market Restaurant</td>
+            <td>Quick, Mobile</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Clarabelle's</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Gibson Girl Ice Cream Parlor</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Blue Bayou Restaurant</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Rancho del Zocalo Restaurante</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Daisy's Diner</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Carnation Cafe</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Milk Stand</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Harbour Galley</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Refreshment Corner</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Royal Street Veranda</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Alien Pizza Planet</td>
+            <td>Table, Quick</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Bengal Barbecue</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Stage Door Cafe</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Cafe Orleans</td>
+            <td>Table</td>
+            <td>$$</td>
+            </tr>
+            <tr>
+            <td>Edelweiss Snacks</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Troubadour Tavern</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Jolly Holiday Bakery Cafe</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Maurice's Treats</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Tiki Juice Bar</td>
+            <td>Quick</td>
+            <td>$</td>
+            </tr>
+            <tr>
+            <td>Plaza Inn</td>
+            <td>Quick, Table</td>
+            <td>$$</td>
+            </tr>
+        </tbody>
+        </Table>
+    </>
     )
 }
