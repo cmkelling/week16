4ef4a017d2fa072430b1c5943d50f8af83c98170
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-14 19:32:49 -0500

    commit

--- finalproject/src/components/hotel.js
+++ finalproject/src/components/hotel.js
@@ -27,9 +27,9 @@ export default function Hotel() {
             <Card style={{ width: '30rem' }}>
             <Card.Img variant="top" src='https://www.disneytouristblog.com/wp-content/uploads/2017/07/grand-californian-lobby-fourth-floor.jpg' alt='Inside the hotel' />
             <Card.Body className='card'>
-                <Card.Title>Exterior</Card.Title>
+                <Card.Title>Interior</Card.Title>
                 <Card.Text className='card-body'>
-                The Grand Californian is one of 3 Resort Hotels to choose from. The most expensive option, and the closest to the park, offers unique craftsman style and unique benefits.
+                Many amenities are included in your stay. Some perks to highlight are the spa, themed pools, direct access into Disneyland, and next day package delivery. Many dining options are available inside or nearby in Downtown Disney.
                 </Card.Text>
             </Card.Body>
             </Card>
@@ -37,9 +37,9 @@ export default function Hotel() {
             <Card style={{ width: '30rem' }}>
             <Card.Img variant="top" src='https://cache.undercovertourist.com/hotel-rooms/disneys-grand-californian-room-96805-18992-16b230c0da5.JPG' alt='Grand Californian Exterior' />
             <Card.Body className='card'>
-                <Card.Title>Exterior</Card.Title>
+                <Card.Title>Room Snapshot</Card.Title>
                 <Card.Text className='card-body'>
-                The Grand Californian is one of 3 Resort Hotels to choose from. The most expensive option, and the closest to the park, offers unique craftsman style and unique benefits.
+                Rooms vary depending on which package you choose. Like most hotels, the Grand Californian offers both 2 Queen and 1 King with a day bed. The view off the balcony changes depending on your package and lookout onto many unique and beautiful views.
                 </Card.Text>
             </Card.Body>
             </Card>
