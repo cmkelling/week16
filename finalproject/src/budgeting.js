1d7c333737553018a3618ff33cd24df122b9a400
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-15 19:34:50 -0500

    commit

--- finalproject/src/budgeting.js
+++ finalproject/src/budgeting.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import BudgetForm from './components/budgetForm';
+
 function Budgeting() {
 
     return(
-        <h2>Budgeting</h2>
+        <>
+            <h2>Budgeting</h2>
+            <BudgetForm/>
+        </>
     )
 
 }
