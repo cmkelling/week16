b9e7542f872369c3da188132e67d718febf90e4b
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-16 17:04:36 -0500

    commit

---
+++ finalproject/src/components/rideList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+
+export default function List (){
+    const [Rides, setRides] = useState([]);
+
+    let apiEndpoint = 'https://631d211d789612cd07a69e37.mockapi.io/v1/Rides';
+
+    let ridesArray = [];
+
+    const getRides = async () => {
+        try {
+          const resp = await fetch(apiEndpoint);
+          const data = await resp.json();
+          console.log(data);
+          
+          return data;
+        } catch (error) {
+          console.log("Oh no! There was an error with getting your ride.", error);
+        }
+    };
+
+    useEffect(
+        () => {
+            // setRides(getRides());
+            ridesArray.push(getRides());
+            setRides(ridesArray)
+            console.log("test2", ridesArray[0]);
+        }, []
+    )
+    
+    
+    return (
+        <div>
+            Test
+            {Rides.map((ride) => {<div>{ride.name} Test1</div>})}
+        </div>
+
+    )
+}
--- finalproject/src/rides.js
+++ finalproject/src/rides.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import List from './components/rideList';
 
 function Rides() {
 
     return(
+       <> 
         <h2>Rides</h2>
+        <List/>
+        </>
     )
 }
 
