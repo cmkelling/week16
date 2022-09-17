df1b5ce1b56c6f9e5a5feadff43aa5ff9002ca47
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-17 13:15:26 -0500

    update form made

--- finalproject/src/components/rideUpdateForm.js
+++ finalproject/src/components/rideUpdateForm.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Button, Form, FormGroup, Row } from 'react-bootstrap';
 
 export default function RideUpdate () {
 
     return(
-        <Form>
-            <FormGroup className='mb-3' controlId='rideName'>
+        <Form className='container'>
+            <Row>
+            <FormGroup className='col-6' controlId='rideName'>
                     <Form.Label>Ride Name</Form.Label>
                     <Form.Control type='text' placeholder='Ride Name as Listed'/>
             </FormGroup>
-            <FormGroup className='mb-3' controlId='rideTime'>
+            <FormGroup className='col-6' controlId='rideLine'>
+                    <Form.Label>Is there a special line?</Form.Label>
+                    <Form.Check/>
+            </FormGroup>
+            </Row>
+            <Row>
+            <FormGroup className='col-6' controlId='rideTime'>
                     <Form.Label>Average Ride Times</Form.Label>
                     <Form.Control type='number'/>
             </FormGroup>
-            <FormGroup className='mb-3' controlId='rideLine'>
-                    <Form.Label>Is there a special line?</Form.Label>
-                    <Form.Control type='checkbox' checked={ true }/>
-            </FormGroup>
-            <FormGroup className='mb-3' controlId='rideWill'>
+            <FormGroup className='col-6' controlId='rideWill'>
                     <Form.Label>Average Time Willing to Wait</Form.Label>
                     <Form.Control type='number'/>
             </FormGroup>
+            </Row>
+            <br/>
+            <Button variant="info" type="submit" value="input">Update</Button>
         </Form>
     )
 }
--- finalproject/src/rides.js
+++ finalproject/src/rides.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import List from './components/rideList';
+import RideUpdate from './components/rideUpdateForm';
 
 function Rides() {
 
@@ -8,6 +9,7 @@ function Rides() {
        <> 
         <h2>Rides</h2>
         <List/>
+        <RideUpdate/>
         </>
     )
 }
