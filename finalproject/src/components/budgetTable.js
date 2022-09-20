dfd5160d9781b902878e5a1fcb6228bfdc77a1fa
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-19 19:34:23 -0500

    commit

---
+++ finalproject/src/components/budgetTable.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BudgetForm from './budgetForm';
+
+export default function BudgetTable() {
+    return(
+        <div>
+            const [budgetData, setBudgetData] = useState();
+        </div>
+    )
+}
