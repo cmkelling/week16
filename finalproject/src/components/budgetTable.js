569b2859b4af13666ea0de19ee483f68dd9eca29
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-09-26 18:14:38 -0500

    budget table

--- finalproject/src/components/budgetTable.js
+++ finalproject/src/components/budgetTable.js
@@ -2,9 +2,57 @@ import React from 'react';
 import BudgetForm from './budgetForm';
 
 export default function BudgetTable() {
+    const [budgetData, setBudgetData] = useState();
+
+    const tableRows = budgetData.map((info) => { 
+        return (
+            <tr>
+                <td>{info.id}</td>
+                <td>{info.name}</td>
+                <td>{info.total}</td>
+                <td>{info.plane}</td>
+                <td>{info.hotel}</td>
+                <td>{info.park}</td>
+                <td>{info.hopper}</td>
+                <td>{info.genie}</td>
+                <td>{info.food}</td>
+                <td>{info.lane}</td>
+                <td>{info.souvenirs}</td>
+                <td>{info.extras}</td>
+            </tr>
+        );
+    });
+
+    const addRows = (data) => {
+        const totalBudget = budgetData.length;
+        data.id = totalBudget + 1;
+        const updatedBudgetData = [...budgetData];
+        updatedBudgetData.push(data);
+        setBudgetData (updatedBudgetData);
+    };
+    
     return(
         <div>
-            const [budgetData, setBudgetData] = useState();
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Total</th>
+                        <th>Plane</th>
+                        <th>Hotel</th>
+                        <th>Park</th>
+                        <th>Hopper</th>
+                        <th>Genie</th>
+                        <th>Food</th>
+                        <th>Lane</th>
+                        <th>Souvenirs</th>
+                        <th>Extra</th>
+                    </tr>
+                </thead>
+                <tbody>{tableRows}</tbody>
+            </table>
+            <BudgetForm func={addRows}/>
         </div>
-    )
+    );
 }
